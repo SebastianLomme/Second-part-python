f1e418bccb8c3a58964058018f7194d151af6a00
Sebastian Lomme <sebastian.lomme@gmail.com>
2021-10-28 18:50:25 +0000

    path change

--- files/main.py
+++ files/main.py
@@ -4,7 +4,9 @@ __human_name__ = "files"
 from zipfile import ZipFile
 import os
 
-path = "/workspaces/test/Workspace/files/cache/"
+currentPath = os.path.dirname(__file__)
+path = os.path.join(currentPath, "cache")
+
 
 
 def clean_cache():
@@ -13,10 +15,7 @@ def clean_cache():
     except:
         list = os.listdir(path)
         for item in list:
-            os.remove(f"{path}{item}")
-
-
-# clean_cache()
+            os.remove(os.path.join(path, item))
 
 
 def cache_zip(path_from, path_to):
@@ -24,18 +23,12 @@ def cache_zip(path_from, path_to):
         zipObj.extractall(path_to)
 
 
-# cache_zip("/workspaces/test/Workspace/files/data.zip", path)
-
-
 def cached_files():
     new_list = []
     list = os.listdir(path)
     for item in list:
-        new_list.append(f"{path}{item}")
+        new_list.append(os.path.join(path, item))
     return new_list
-
-
-# print(cached_files())
 
 
 def get_next_word(text, match):
@@ -54,5 +47,3 @@ def find_password(list):
             password = get_next_word(read_file, "password:")
             return password
 
-
-# print(find_password(cached_files()))
---
+++ main.py
@@ -0,0 +1,18 @@
+# def multiply_a(a, b):
+#     return a * b
+
+# def multiply_b(a, b):
+#     return multiply_a(a, b)
+
+# def multiply_c(a, b):
+#     return multiply_b(a, b)
+
+# print(multiply_c(3, 5))
+# print(multiply_c('incoming', 'error'))
+
+
+def divide(a, b):
+    return a / b
+
+
+divide(2, 0)
